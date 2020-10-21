9d60994a94bd0f076a9e274b1c345a347ebac7d7
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-10-21 11:32:00 -0300

    Arrays com dados dos candidatos

---
+++ etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Mario Bros',
+                partido: 'Encanadores',
+                fotos:[
+                    {url: './images/mario.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 30100,
+                nome: 'Donkey Kong',
+                partido: 'Bananas',
+                fotos:[
+                    {url: './images/kong.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Goku Black',
+                partido: 'Extinção',
+                vice: 'Majin Vegeta',
+                fotos:[
+                    {url:'./images/goku-black.jpg', legenda: 'Prefeito', small: false},
+                    {url:'./images/majin-vegeta.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                titulo: 'PREFEITO',
+                numeros: 2,
+                candidatos: [
+                    {
+                        numero: 10,
+                        nome: 'Goku',
+                        partido: 'Saiyajin',
+                        vice: 'Broly',
+                        fotos:[
+                            {url:'./images/goku-migate.jpg', legenda: 'Prefeito', small: false},
+                            {url:'./images/broly.jpg', legenda: 'Vice-Prefeito', small: true}
+                        ]
+                    },
+                ]
+            }
+        ]
+    }
+];
