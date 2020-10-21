527b0f087db0f0d0d7005a9953edc924822b60e6
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-10-21 12:24:14 -0300

    add // variaveis de ambiente

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 let seuVotoPara = document.querySelector('.d-1-1 span');
 
 let cargo = document.querySelector('.d-1-2 span');
-
 let descricao = document.querySelector('.d-1-4');
-
 let aviso = document.querySelector('.d-2');
-
 let lateral = document.querySelector('.d-1-right');
-
 let numeros = document.querySelector('.d-1-3');
 
+// variaveis de controle de interface
+
+// variaveis de ambiente
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i = 0; i < etapa.numero; i++) {
+        if(i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+    }
+}
+
+function atualizaInterface() {
+
+}
+
 function clicou(n) {
-    alert(`clicou em ${n}`)
+    let elNumero = document.querySelector('.numero.pisca'); //El = Elemento
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+    }
 }
 function branco() {
     alert(`clicou em branco`)
@@ -22,3 +54,5 @@ function corrige() {
 function confirma() {
     alert(`clicou em confirma`)
 }
+
+comecarEtapa()
